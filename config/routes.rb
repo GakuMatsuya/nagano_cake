3e56d0e258d0dd94889cfe91ca770bc489accd6e
GakuMatsuya <gaku.1237@icloud.com>
2021-01-20 13:59:04 +0000

    [Add] root

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  
   devise_for :admin
   devise_for :customers
    get '/admin' => 'admin/homes#top'
+  namespace :admin do
+    resources :items, except:[:destroy]
+  end
+
 end
