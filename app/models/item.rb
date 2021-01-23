257a657f6a10e090db5f2804fd6d680399df5722
GakuMatsuya <gaku.1237@icloud.com>
2021-01-23 08:08:35 +0000

    [Add] ジャンルの投稿・表示

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,10 @@
 class Item < ApplicationRecord
 
-  belongs_to :genre, optional: true
+  belongs_to :genre
   has_many :cart_items, dependent: :destroy
   has_many :ordered_items, dependent: :destroy
   attachment :image
+  
+  
 
 end
