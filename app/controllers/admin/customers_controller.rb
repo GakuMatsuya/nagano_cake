b65bef86d0344223c42d178faea17540b79f3af8
GakuMatsuya <gaku.1237@icloud.com>
2021-01-24 06:14:05 +0000

    [Add]customers-controller

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,6 +1,7 @@
 class Admin::CustomersController < ApplicationController
   
   def index
+    @customers = Customer.all
   end
   
   def show
@@ -11,4 +12,5 @@ class Admin::CustomersController < ApplicationController
 
   def update
   end
+  
 end
