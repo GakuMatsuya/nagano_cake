6ed147543db95cd5cb0cffe14eec19f720f50392
GakuMatsuya <gaku.1237@icloud.com>
2021-01-24 02:32:30 +0000

    [Add] customerコントローラ、ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :items, except:[:destroy]
     resources :genres, except:[:show, :destroy]
+    resources :customers
   end
 
 end
