bbce1075aa0a0d7a68acaaa4afa8cd20718a2327
GakuMatsuya <gaku.1237@icloud.com>
2021-01-24 05:24:37 +0000

    [Add] Refile.serect_key

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'bc3fce4426d80e7f6b03c2ee71ca23383a753c476c3f734860dfa79a0dcfadf71b0b79486a5da17223a3f4d15e47bd062b75055eb0cfe76ec4c0b6989354307c'
