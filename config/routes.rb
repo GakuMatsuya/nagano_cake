8e4b4d25f5da974d57ec199ca7b67728febdcfcd
GakuMatsuya <gaku.1237@icloud.com>
2021-01-26 12:03:02 +0000

    管理者ログイン画面のルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
   
-  devise_for :admin
+  devise_for :admin, controllers: {
+    sessions: 'admin/sessions/sessions',
+    passwords: 'admin/sessions/passwords',
+    registrations: 'admin/sessions/registrations'
+  }
+  
+  
+  
+  
+  
   devise_for :customers
    get '/admin' => 'admin/homes#top'
   
