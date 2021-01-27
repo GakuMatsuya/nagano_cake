5dfcf18d3b840b26886faabb8cdf27fb3ad55ebb
GakuMatsuya <gaku.1237@icloud.com>
2021-01-27 12:11:53 +0000

    新規登録のパラメーター追加

--- app/controllers/public/sessions/registrations_controller.rb
+++ app/controllers/public/sessions/registrations_controller.rb
@@ -3,6 +3,7 @@
 class Public::Sessions::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
+  before_action :configure_permitted_parameters
 
   # GET /resource/sign_up
   # def new
@@ -41,9 +42,9 @@ class Public::Sessions::RegistrationsController < Devise::RegistrationsControlle
   # protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute, :last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, :is_active])
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_account_update_params
@@ -59,4 +60,9 @@ class Public::Sessions::RegistrationsController < Devise::RegistrationsControlle
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
   # end
+  
+  def after_sign_up_path_for(resource)
+    customers_my_page_path
+  end
+  
 end
