76a691385dd79543df4222cb023839e1fb2dffd7
GakuMatsuya <gaku.1237@icloud.com>
2021-01-31 06:58:37 +0000

    注文確認画面の追加

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,10 +1,25 @@
 class Public::OrdersController < ApplicationController
-  
+
   def new
     @order = Order.new
+    @cart_items = CartItem.all
+    @sum = 0
   end
-  
+
+  def create
+  end
+
   def confirm
+    @order = Order.new(order_params)
+    @cart_items = CartItem.all
+    @sum = 0
   end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:address, :payment_method, :postal_code, :name, :billing_amount, :shipping_fee)
+  end
+
 
 end
