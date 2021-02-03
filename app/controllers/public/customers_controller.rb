366e1aea03c2e023e974072d0e80cf2a141a63a6
GakuMatsuya <gaku.1237@icloud.com>
2021-02-03 12:13:46 +0000

    編集保存機能の追加

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -9,6 +9,9 @@ class Public::CustomersController < ApplicationController
   end
 
   def update
+    @customer = Customer.find(params[:id])
+    @customer.update(customer_params)
+    redirect_to customers_my_page_path
   end
   
   def unsubscribe
@@ -17,4 +20,11 @@ class Public::CustomersController < ApplicationController
   def withdraw
   end
 
+
+  private
+  
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, :email)
+  end
+  
 end
