a603ec13d2c6df449fc97f57d7006c7a44eedf40
GakuMatsuya <gaku.1237@icloud.com>
2021-02-06 00:45:11 +0000

    会員退会機能の追加

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,9 +1,9 @@
 class Public::CustomersController < ApplicationController
-  
+
   def show
-    @customer = current_customer 
+    @customer = current_customer
   end
-  
+
   def edit
     @customer = Customer.find(params[:id])
   end
@@ -13,18 +13,22 @@ class Public::CustomersController < ApplicationController
     @customer.update(customer_params)
     redirect_to customers_my_page_path
   end
-  
+
   def unsubscribe
+    @customer = current_customer
   end
 
   def withdraw
+    @customer = current_customer
+    @customer.update(customer_params)
+    redirect_to "/"
   end
 
 
   private
-  
+
   def customer_params
-    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, :email)
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, :email, :is_active)
   end
-  
+
 end
