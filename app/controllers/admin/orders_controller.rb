ed9da8c4b03f0b622dea5963aef18b5d4d117c8f
GakuMatsuya <gaku.1237@icloud.com>
2021-02-07 05:39:01 +0000

    注文履歴詳細ページの追加

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,13 +1,17 @@
 class Admin::OrdersController < ApplicationController
-  
+
   before_action :authenticate_admin!
-  
+
   def show
+    @order = Order.find(params[:id])
+    @ordered_items = @order.ordered_items
+    @customer = Customer.find(@order.customer_id)
+    @sum = 0
   end
 
   def update
   end
-  
- 
-  
+
+
+
 end
