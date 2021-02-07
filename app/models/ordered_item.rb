28cd78b431e64021d3125197eec94dbb966645cd
GakuMatsuya <gaku.1237@icloud.com>
2021-02-07 00:54:26 +0000

    enumの日本語化

--- app/models/ordered_item.rb
+++ app/models/ordered_item.rb
@@ -4,7 +4,7 @@ class OrderedItem < ApplicationRecord
     cannot_be_started:       0, 
     waiting_for_production:  1, 
     production:              2, 
-    製作完了:3
+    production_completed:    3,
   }
 
   belongs_to :order
