95ef1d0134d855193432f12c5d5795940f245cb1
GakuMatsuya <gaku.1237@icloud.com>
2021-02-08 13:15:19 +0000

    注文履歴詳細の追加

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -8,7 +8,12 @@ class Public::OrdersController < ApplicationController
   end
 
   def index
-    @orders = current_customer.orders
+    @orders = current_customer.orders 
+  end
+
+  def show
+    @order = Order.find(params[:id])
+    @ordered_items = OrderedItem.all
   end
 
   def create
