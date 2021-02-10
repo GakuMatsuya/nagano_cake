0c77b830e7550ef7eddf80225ab90d5ea7e73869
GakuMatsuya <gaku.1237@icloud.com>
2021-02-10 07:36:02 +0000

    ステータス更新機能

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,11 +1,23 @@
 class Admin::OrderDetailsController < ApplicationController
 
-   before_action :authenticate_customer!
+   before_action :authenticate_admin!
 
   def update
     @ordered_item = OrderedItem.find(params[:id])
     @order = Order.find(@ordered_item.order_id)
-    @ordered_item.update(ordered_item_params)
+      if @ordered_item.status != ordered_item_params[:status]
+        @ordered_item.update(status: ordered_item_params[:status])
+        if @ordered_item.status == "production"
+          @order.update(status: 2)
+        end
+      end
+    
+    a = @order.ordered_items.where(status: "production_completed")
+    b = @order.ordered_items
+    
+    if a.count == b.count
+      @order.update(status: 3)
+    end
     redirect_to admin_order_path(@order)
   end
 
@@ -14,5 +26,5 @@ class Admin::OrderDetailsController < ApplicationController
   def ordered_item_params
     params.require(:ordered_item).permit(:status)
   end
-
+  
 end
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -11,16 +11,24 @@ class Admin::OrdersController < ApplicationController
 
   def update
     @order = Order.find(params[:id])
-    @order.update(ordered_params)
+      if @order.status != order_params[:status]
+        @order.update(status: order_params[:status])
+      if @order.status == "payment_confirmation"
+        @order.ordered_items.each do |ordered_item|
+          ordered_item.update(status: 1)
+        end
+      end
+      end
+  
     redirect_to admin_order_path(@order)
   end
 
   private
 
-  def ordered_params
+  def order_params
    params.require(:order).permit(:status)
   end
 
- 
+
 
 end
