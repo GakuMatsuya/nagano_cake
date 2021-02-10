f8448baf6558acbeb37522ef656faed9c68992a4
GakuMatsuya <gaku.1237@icloud.com>
2021-02-10 07:37:25 +0000

    バグ修正

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,10 +1,10 @@
 class Admin::HomesController < ApplicationController
-  
+
   before_action :authenticate_admin!
 
   def top
     @orders = Order.all
-    #@ordered_item = order.id.oedered_item
+    @ordered_items = OrderedItem.all
     @sum = 0
   end
 
